30202deac633e4b30df88d16bfe68d43986e28ea
Simon Eisenmann <simon@longsleep.org>
2020-07-14 20:41:26 +0200

    Show icons in rofi

--- rofi-window.sh
+++ rofi-window.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 #exec rofi -show window -eh 1
-exec rofi -combi-modi window,drun -show combi -modi combi -eh 1 -font "Fira Mono for Powerline 12" -dpi 180 
+exec rofi -combi-modi window,drun -show combi -modi combi -show-icons -eh 1 -font "Fira Mono for Powerline 12" -dpi 180 
