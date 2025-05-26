91607af283a1cac96a4c62dc02936de97e437e51
Simon Eisenmann <simon@longsleep.org>
2025-05-26 10:31:37 +0200

    Add fish, helix and starship config

---
+++ config/config.fish
@@ -0,0 +1,13 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+set -g fish_greeting
+
+
+set -Ux EDITOR "hx"
+set -Ux EMAIL "simon@longsleep.org"
+
+set -Ux WASMTIME_HOME "$HOME/.wasmtime"
+
+#starship init fish | source
