cda64c0e407ea383aff26edb63bffc7e20f7d5a3
Simon Eisenmann <simon@longsleep.org>
2026-03-11 12:19:04 +0100

    Update fish config

--- config/config.fish
+++ config/config.fish
@@ -4,10 +4,22 @@ end
 
 set -g fish_greeting
 
-
-set -Ux EDITOR "hx"
+set -Ux EDITOR hx
+#set -Ux EDITOR "vim"
 set -Ux EMAIL "simon@longsleep.org"
 
 set -Ux WASMTIME_HOME "$HOME/.wasmtime"
 
 #starship init fish | source
+
+# bun
+set --export BUN_INSTALL "$HOME/.bun"
+set --export PATH $BUN_INSTALL/bin $PATH
+
+# opencode
+fish_add_path /home/longsleep/.opencode/bin
+
+# Added by LM Studio CLI (lms)
+set -gx PATH $PATH /home/longsleep/.lmstudio/bin
+# End of LM Studio CLI section
+
